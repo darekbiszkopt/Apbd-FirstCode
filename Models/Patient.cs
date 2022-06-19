803f08855998e89ce70993b85315fe65385a1243
Darek <daarek.dajcz@gmail.com>
2022-06-19 13:16:02 +0200

    seeding data

---
+++ Models/Patient.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Entity6._0Solution.Models
+{
+    public class Patient
+    {
+        [Key]
+        public int IdPatient { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string FirstName { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string LastName { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public DateTime Birthday { get; set; }
+
+        public ICollection<Prescription> presciptions { get; set; }
+    }
+}
+
